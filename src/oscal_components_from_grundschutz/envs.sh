2d646338981e70acb812962131434871dd0cb4ad
Christoph Puppe / NTT <christoph.puppe@nttdata.com>
2025-06-26 12:15:16 +0000

    one was missing

--- src/oscal_components_from_grundschutz/envs.sh
+++ src/oscal_components_from_grundschutz/envs.sh
@@ -6,7 +6,7 @@
 export GCP_PROJECT_ID="ai-ensemble"
 export BUCKET_NAME="us_rag_storage_1"
 export EXISTING_JSON_GCS_PATH="results/BSI_GS_OSCAL_current_2023_benutzerdefinierte.json"
-export OUTPUT_PREFIX="components/"
+export OUTPUT_PREFIX="components_test/"
 
 export TEST=true
 echo "Environment variables set for main.py. You can now run the script."
